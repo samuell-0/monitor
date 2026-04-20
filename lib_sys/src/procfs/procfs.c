8f25ef2ed1f70598e0e7497b90e75dc2f638f4b6
Samuel Fikadu <samuelfikadumamo@gmail.com>
2026-04-20 17:09:12 +0300

    oppsie

--- lib_sys/src/procfs/procfs.c
+++
@@ -1,9 +0,0 @@
-#include "procfs.h"
-#include "lib_sys.h"
-#include <stdio.h>
-
-uint8_t parse_cpu_usage(struct cpu_struct* cpu_struct_ptr)
-{
-    // cpu_struct_ptr->name = "my cpu";
-    printf("__\n");
-}
--- lib_sys/src/procfs/procfs.h
+++
@@ -1,2 +0,0 @@
-#include "lib_sys.h"
-uint8_t parse_cpu_usage(struct cpu_struct* cpu_struct_ptr);
