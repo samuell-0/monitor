29b5dd361594aef6e9ca09b6e4cb02c71aa80ffb
Samuel Fikadu <samuelfikadumamo@gmail.com>
2026-04-26 09:37:27 +0300

    mem added and improved cpu

--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,23 @@
 
 // the first value is always a pointer to a function that take 
 // an array of void pointers(this are the arguments of the update function which the user interpreats)
-struct ui_data
+struct usr_data
 {
     void (*update_func)(void*);
-    void** args;
-    uint8_t n_args;
+    slint::ComponentHandle<AppWindow>* ui_ptr;
     struct log_data* log_data_ptr;
 };
 // intead use slint's even callbacks in the main thread(kepping this in c case i need it)
 void update_stats(void* usr_data_ptr)
 {
-    struct ui_data* ui_data_ptr = (struct ui_data*)(usr_data_ptr);
-    if (ui_data_ptr->args ==  NULL)
-        return;
-    if (ui_data_ptr->args[0] != NULL)
-        slint::invoke_from_event_loop([ui_data_ptr]()
+    struct usr_data* _usr_data_ptr = (struct usr_data*)usr_data_ptr;
+    if (_usr_data_ptr->ui_ptr != NULL)
+        slint::invoke_from_event_loop([_usr_data_ptr]()
             {
-                (*((slint::ComponentHandle<AppWindow>*)(ui_data_ptr->args[0])))->set_cpu_usage(ui_data_ptr->log_data_ptr->cpu_info_ptr->usage);
+                (*_usr_data_ptr->ui_ptr)->
+                    set_cpu_usage(_usr_data_ptr->log_data_ptr->cpu_info_ptr->usage_percent);
+                (*_usr_data_ptr->ui_ptr)->
+                    set_mem_usage(_usr_data_ptr->log_data_ptr->mem_info_ptr->usage_percent);
             }
         );
 }
@@ -29,18 +29,16 @@ int main(int argc, char **argv)
     auto ui = AppWindow::create();
 
     struct log_data _log_data{0};
-    struct ui_data _ui_data{0};
-    void* arr[1]{0};
+    struct usr_data _usr_data{0};
 
-    _ui_data.log_data_ptr = &_log_data;
-    _ui_data.update_func = update_stats;
-    _ui_data.args = arr;
-    _ui_data.n_args = 1;
-    _ui_data.args[0] = &ui;
+    _log_data.usr_data_ptr = &_usr_data;
+    _usr_data.log_data_ptr = &_log_data;
+    _usr_data.ui_ptr = &ui;
+    _usr_data.update_func = update_stats;
     
-    _log_data.usr_data_ptr = &_ui_data;
     
     lib_sys_log_cpu(&_log_data);
+    lib_sys_log_mem(&_log_data);
 
     thrd_t thread_ptr = lib_sys_log(&_log_data);
 
