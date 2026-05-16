4880bde70bee6b19c5663cd235b289915f03a618
Samuel Fikadu <samuelfikadumamo@gmail.com>
2026-05-16 12:25:51 +0300

    working make_graph_path()...from Graph

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include "main.h"
 #include "lib_sys.h"
+#include "graph_path.h"
+#include <cstring>
 
 
 // the first value is always a pointer to a function that take 
@@ -9,20 +11,31 @@ struct usr_data
     void (*update_func)(void*);
     slint::ComponentHandle<AppWindow>* ui_ptr;
     struct log_data* log_data_ptr;
+    char cpuG_draw_cmd[SIZE_CMD];
+    char memG_draw_cmd[SIZE_CMD];
+    uint8_t last_percentage;
 };
 // intead use slint's even callbacks in the main thread(kepping this in c case i need it)
 void update_stats(void* usr_data_ptr)
 {
+    static uint8_t i{0};
     struct usr_data* _usr_data_ptr = (struct usr_data*)usr_data_ptr;
-    // if (_usr_data_ptr->ui_ptr != NULL)
-    //     slint::invoke_from_event_loop([_usr_data_ptr]()
-    //         {
-    //             (*_usr_data_ptr->ui_ptr)->
-    //                 set_cpu_usage(_usr_data_ptr->log_data_ptr->cpu_info_ptr->usage_percent);
-    //             (*_usr_data_ptr->ui_ptr)->
-    //                 set_mem_usage(_usr_data_ptr->log_data_ptr->mem_info_ptr->usage_percent);
-    //         }
-    //     );
+    if (_usr_data_ptr->ui_ptr != NULL)
+        slint::invoke_from_event_loop([_usr_data_ptr]()
+            {
+                // make_graph_path(_usr_data_ptr->cpuG_draw_cmd, _usr_data_ptr->log_data_ptr->cpu_info_ptr->usage_percent);
+                // (*_usr_data_ptr->ui_ptr)->
+                //     set_cpuG_draw_cmd(_usr_data_ptr->cpuG_draw_cmd);
+
+                make_graph_path(_usr_data_ptr->memG_draw_cmd, _usr_data_ptr->log_data_ptr->mem_info_ptr->usage_percent);
+                (*_usr_data_ptr->ui_ptr)->
+                    set_memG_draw_cmd(_usr_data_ptr->cpuG_draw_cmd);
+            }
+        );
+}
+void init(struct usr_data* usr_data_ptr, struct log_data* log_data_ptr)
+{
+
 }
 int main(int argc, char **argv)
 {
@@ -31,17 +44,18 @@ int main(int argc, char **argv)
     struct log_data _log_data{0};
     struct usr_data _usr_data{0};
 
-    _log_data.usr_data_ptr = &_usr_data;
-    _usr_data.log_data_ptr = &_log_data;
-    _usr_data.ui_ptr = &ui;
-    _usr_data.update_func = update_stats;
-    
+    _log_data.usr_data_ptr  = &_usr_data;
+    _usr_data.ui_ptr        = &ui;
+    _usr_data.update_func   = update_stats;
+    _usr_data.log_data_ptr  = &_log_data;
+
+    memset(_usr_data.cpuG_draw_cmd, ' ', SIZE_CMD);
+    memset(_usr_data.memG_draw_cmd, ' ', SIZE_CMD);
     
     lib_sys_log_cpu(&_log_data);
     lib_sys_log_mem(&_log_data);
 
     thrd_t thread_ptr = lib_sys_log(&_log_data);
-
 
     ui->run();
     
